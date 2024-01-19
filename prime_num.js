5ca8e0a96c9766c6dcf53283bd0ac979f3fc3e11
sangavi002 <sangavibabu007@gmail.com>
2024-01-19 23:43:06 +0530

    prime number

---
+++ prime_num.js
@@ -0,0 +1,18 @@
+function primeNumber(num){
+    let primeFlag = true;
+    if (num <= 1){
+        primeflag=false;
+    }
+    else{
+        for(i=2;i<num;i++){
+            if(num % i == 0){
+                primeFlag=true;
+            }
+        }
+    }
+
+    console.log(primeFlag);
+    
+}
+
+primeNumber(5);
